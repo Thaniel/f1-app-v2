97c4638fefa1a3b38b12a68ea0b313b2d293987e
Daniel Herraez <danielhp0905@gmail.com>
2024-08-11 16:07:09 +0200

    Added teams details

--- src/app/pages/teams/team-details/team-details.component.ts
+++ src/app/pages/teams/team-details/team-details.component.ts
@@ -1,30 +1,67 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { IDriver } from '../../../core/interfaces/driver.interface';
 import { ITeam } from '../../../core/interfaces/team.interface';
+import { TeamsService } from '../../../core/services/teams/teams.service';
 import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-team-details',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, CommonModule, RouterLink],
   templateUrl: './team-details.component.html',
   styleUrl: './team-details.component.css'
 })
-export class TeamDetailsComponent {
-  team: ITeam =
-  {
-    id: '1',
-    name: "Mercedes",
-    fullName: "Mercedes-AMG Petronas Formula One Team",
-    teamPrincipal: "Toto Wolff",
-    titles: 7,
-    points: 1000,
-    colorCode: "#00D2BE",
+export class TeamDetailsComponent implements OnInit {
+
+  teamSelected: ITeam = {
+    id: '0',
+    name: '',
+    fullName: '',
+    teamPrincipal: '',
+    titles: 0,
+    points: 0,
+    colorCode: '',
     driver1: null,
     driver2: null,
-    description: "One of the most successful teams in Formula 1 history.",
+    description: '',
     carImage: null,
-    carImageUrl: "",
+    carImageUrl: '',
     logoImage: null,
-    logoImageUrl: "",
+    logoImageUrl: '',
+  };
+
+  driver1: IDriver | null = null;
+  driver2: IDriver | null = null;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private teamsService: TeamsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = params.get('id');
+      this.getTeamDetail(id);
+    });
+  }
+
+  private async getTeamDetail(id: string | null) {
+    if (id != null) {
+      let data = await this.teamsService.getById(id)
+
+      if (data != null) {
+        this.teamSelected = data;
+        this.driver1 = this.teamSelected.driver1 as IDriver;
+        this.driver2 = this.teamSelected.driver2 as IDriver;
+      } else {
+        this.router.navigate(['/teams']);
+      }
+    } else {
+      this.router.navigate(['/teams']);
+    }
   }
 }
